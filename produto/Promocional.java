ae4ca4a80ac3cd5ed57a51678ab05a9c0d7ae6af
Lucas Ferreira dos santos <lucas.santos61@fatec.sp.gov.br>
2016-07-04 09:44:06 -0300

    Delete Promocional.java

--- produto/Promocional.java
+++
@@ -1,8 +0,0 @@
-package br.com.casadocodigo.produto;
-
-public interface Promocional {
-	 public boolean aplicaDesconto(double desc);
-	 default boolean aplicaDesconto10(){
-		 return aplicaDesconto(0.1);
-	 }
-}
