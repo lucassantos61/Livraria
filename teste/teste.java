d4c8b0d2e23f53757ca101d5c2558f5c0cdd9303
Lucas Ferreira dos santos <lucas.santos61@fatec.sp.gov.br>
2016-07-04 09:42:34 -0300

    Delete teste.java

--- teste/teste.java
+++
@@ -1,8 +0,0 @@
-package br.com.casadocodigo.teste;
-import br.com.casadocodigo.livraria.Autor;
-import br.com.casadocodigo.produto.Livro;
-public class teste {
-	public static void main(String [] Args){
-		
-	}
-}
